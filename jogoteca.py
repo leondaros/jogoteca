713b40a633f249b0c4387e5617d7f915ca8893d1
Leon Daros <leondaros@gmail.com>
2024-08-30 22:14:51 +0100

    Adjust page redirect

--- jogoteca.py
+++ jogoteca.py
@@ -23,7 +23,7 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect('/login')
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route("/criar", methods=['POST'])
@@ -37,14 +37,16 @@ def criar():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route("/autenticar", methods=['POST'])
 def autenticar():
     if 'alohomora' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
     flash('Usuário não logado')
     return redirect('/login')
 
