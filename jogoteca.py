79abb6c8f894886e5ea367ac370626b420bb90cc
Leon Daros <leondaros@gmail.com>
2024-08-31 22:40:01 +0100

    Improving page redirect

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 
 class Jogo:
@@ -23,7 +23,7 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect('/login?proxima=novo')
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route("/criar", methods=['POST'])
@@ -33,7 +33,7 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route("/login")
 def login():
@@ -46,14 +46,14 @@ def autenticar():
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + ' logado com sucesso!')
         proxima_pagina = request.form['proxima']
-        return redirect('/{}'.format(proxima_pagina))
+        return redirect(proxima_pagina)
     flash('Usuário não logado')
-    return redirect('/login')
+    return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
