b38eb50dabac1e9619b37a68f5d2b64dba608e5c
Leon Daros <leondaros@gmail.com>
2024-09-04 00:38:22 +0100

    Cretae

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 class Jogo:
     def __init__(self,nome, categoria, console):
@@ -28,6 +28,17 @@ usuarios = { usuario1.nickname:usuario1,
 
 app = Flask(__name__)
 app.secret_key='jogoteca'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'password',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
