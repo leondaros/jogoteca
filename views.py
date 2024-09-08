d7ad61b70d6a9adf036be18207b9c473b203954c
Leon Daros <leondaros@gmail.com>
2024-09-08 21:44:26 +0100

    Delete items from list

--- views.py
+++ views.py
@@ -45,6 +45,15 @@ def atualizar():
     db.session.commit()
     return  redirect(url_for('index'))
 
+@app.route("/deletar/<int:id>")
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login'))
+    Jogos.query.filter_by(id=id).delete()
+    db.session.commit()
+    flash('Jogo deletado com sucesso!')
+    return redirect(url_for('index'))
+
 @app.route("/login")
 def login():
     proxima = request.args.get('proxima')
